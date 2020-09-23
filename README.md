007ee9bbe6dadb40c5a4d88ab5f0f2e1056f360c
Shaquille Smith <ssmith01850@gmail.com>
2020-09-23 17:16:51 -0400

    #0002 - Fixed the logic, now the code loops through 5 times. However, I need to modifty the script so that it provides the player can have different choices for each round instead of using the same selection from the beginning. Additionally, I still need to fix to validator function as well. Also, the logic needs to be reworked for comparing the overall points for the computer and the player.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Rock-Paper-Scissors
+A simple web application that demonstrates my proficiency in HTML5, CSS3, and Javascript.
--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 console.log(`Time to play Rock, Paper, Scissors!`); // This line is to let the user know that the game has started
-let pcCounter = 0;
-let playerCounter = 0;
+let cpoints = 0;
+let ppoints = 0;
 const playerChoice = prompt(`Choose between Rock, Paper, and Scissors: `).toLowerCase(); //This line is a prompt to allow the user to make a choice
 const pcChoice = ['rock', 'paper', 'scissors']; //This line stores the choices avaiable to the computer
+console.log(`This is before the game function`);
+game(playerChoice, pcChoice, cpoints, ppoints);
+//playRound(playerChoice, pcChoice);
 
-function game(playerChoice, pcChoice, pcCounter, playerCounter){ //This is a for loop counter which is supposed to create 5 rounds of the game.
-  for (i = 0; i > 5; ++i){ //Not really sure why the code isn't looping though
-    if(pcCounter >= 5){
+function game(playerChoice, pcChoice, cpoints, ppoints){ //This is a for loop counter which is supposed to create 5 rounds of the game.
+  console.log(`You are now in: game()`);
+  for (i = 0; i < 5; i++){ //Not really sure why the code isn't looping though; UPDATE: Had the wrong operator ;P
+    console.log(`You are now in: game() - for loop`);
+    if(cpoints >= 5){
       console.log(`You Lose!`);
-    }else if (playerCounter >= 5){
+    }else if (ppoints >= 5){
       console.log(`You Win!`);
     }else playRound(playerChoice, pcChoice);
   }
 }
 
-function random(min,max) { // The function creates a random number within the array
+/*function random(min,max) { // The function creates a random number within the array
+    console.log(`You are in the random function`);
     const num = Math.floor(Math.random() * (max - min)) + min;
     return num;
-  }
-
+  }// Found a cleaner method that combines both random and computerPlay
+*/
 function computerPlay($pcChoice){ // This function generates a random choice using the random function
-    let choice = pcChoice[random(0, pcChoice.length)]; // The random choice is generated here
+    console.log(`You are in the computer play function`);
+    let choice = pcChoice[Math.floor((Math.random()*3))]; // The random choice is generated here; Updated the selection process in this function
     return choice; // Then the choice is outputted through here
   }
 
@@ -33,38 +40,40 @@ function computerPlay($pcChoice){ // This function generates a random choice usi
 */
 
 function playRound(playerSelection, computerSelection){
+  console.log(`You are in the playRound function`);
   //validate(playerChoice);
   computerSelection = computerPlay();
   playerSelection = playerChoice;
   if (playerSelection === `rock` && computerSelection === `paper`){
       console.log(`You Lose! Paper beats Rock!`);
-      ++pcCounter;
+      cpoints++;
   }else if (playerSelection === `rock` && computerSelection === `rock`){
       console.log(`Tie Game!`);
   }else if (playerSelection === `rock` && computerSelection === `scissors`){
     console.log(`You Win! Rock beats Scissors!`);
-    ++playerCounter;
+    ppoints++;
   }
 
   if (playerSelection === `paper` && computerSelection === `scissors`){
       console.log(`You Lose! Scissors beats Paper!`);
-      ++pcCounter;
+      cpoints++;
   }else if (playerSelection === `paper` && computerSelection === `paper`){
       console.log(`Tie Game!`);
   }else if (playerSelection === `paper` && computerSelection === `rock`){
       console.log(`You Win! Paper beats Rock!`);
-      ++playerCounter;
+      ppoints++;
   }
   
   if (playerSelection === `scissors` && computerSelection === `rock`){
       console.log(`You Lose! Rock beats Scissors!`);
-      ++pcCounter;
+      cpoints++;
   }else if (playerSelection === `scissors` && computerSelection === `scissors`){
       console.log(`Tie Game!`);
   }else if (playerSelection === `scissors` && computerSelection === `paper`){
       console.log(`You Win! Scissors beats Paper!`);
-      ++playerCounter;
+      ppoints++;
   }
+}
 
   /*validate(playerSelection); // Old switch statement meant for decision making
   console.log("This is the playRound function!")
@@ -117,5 +126,3 @@ function playRound(playerSelection, computerSelection){
       console.log(`Tie`);
       break;  
   }*/
-}
-game(playerChoice, pcChoice, pcCounter, playerCounter);
